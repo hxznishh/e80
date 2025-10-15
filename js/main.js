1c89ce429e044584a54e13572c60e701a8237d0c
hxznishh <noobyrblx7@gmail.com>
2025-10-15 22:24:57 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAFI GAY').split('')
+    const titles = ('HAFI GAY ???').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
